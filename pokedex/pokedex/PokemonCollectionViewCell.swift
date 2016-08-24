81195ad94dfa0839c3b2b108daeac528b843a526
JeanChRocha <jeanchrocha@gmail.com>
2016-08-24 20:06:29 -0300

    iniciado layout da primeira tela; alterado status bar para aparecer branca

---
+++ pokedex/pokedex/PokemonCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PokemonCollectionViewCell.swift
+//  pokedex
+//
+//  Created by Jean Aparecido Chaves da Rocha on 24/08/16.
+//  Copyright © 2016 FIAP. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var pokeImage: UIImageView!
+}
